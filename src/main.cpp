ff73b82eb07cc6072bb4e073302cf00456034e96
Romain CHAHINE <romain.chahine@rwigo.com>
2022-03-18 11:41:21 +0100

    init shitcode

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,74 @@
 #include <Arduino.h>
 #include <AutonomousBonsai.h>
-#include <MQTTLib.h>
- 
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+#include <string>
 unsigned long lastTime;
 
-MQTTLib mqttClient;
+WiFiClient espClient;
+PubSubClient MQTTClient(espClient);
 
-void setup() {
+void setup_wifi()
+{
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("iPhone 11", "123456789");
+
+  delay(10000);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(WiFi.status());
+  }
+}
+
+void setup()
+{
   Serial.begin(115200);
   lastTime = millis();
 
+  Serial.println("Setup WIFI");
+  setup_wifi();
+
+  Serial.println("Setup MQTT Server");
+  MQTTClient.setServer("172.20.10.10", 1883);
+
+  Serial.println("Setup PinMode");
   pinMode(GPIO_MOISTURE_SENSOR, INPUT);
-
-  mqttClient.init();
-
 }
 
-void loop() {
+void loop()
+{
   const unsigned long currentTime = millis();
 
-  if((currentTime - lastTime) >= 30000) {
-    
-    int analog_value = analogRead(GPIO_MOISTURE_SENSOR);
-    uint16_t moistVal = map(analog_value, 0, 1023, 100, 0);
+  if ((currentTime - lastTime) >= 3000)
+  {
+    int attempts = 0;
 
-    mqttClient.publish_moisture(moistVal);
+    while (!MQTTClient.connected() && attempts < 5)
+    {
+      Serial.println("Trying to connect MQTT");
+
+      if (MQTTClient.connect("ESP8266Client"))
+      {
+
+        uint16_t const moistVal = map(analogRead(GPIO_MOISTURE_SENSOR), 0, 1023, 100, 0);
+        std::string s = std::to_string(moistVal);
+
+        if (MQTTClient.publish("bonsai/moisture", s.c_str()))
+        {
+          Serial.println("Published on bonsai/moisture");
+        }
+        else
+        {
+          Serial.println("Error during publish");
+        }
+      }
+      delay(500);
+      attempts++;
+    }
+
     lastTime = currentTime;
   }
-
 }
